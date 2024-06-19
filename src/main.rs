810105dd96fda6db3ad776d3392cccf56ade97a4
theo <lesgooo1993@proton.me>
2024-06-19 15:06:04 +0530

    checks if .config/bdaylog and db are present; if not, it creates them

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use rusqlite::{Connection, Result};
+use std::env::var;
+use std::path::Path;
 
 #[derive(Debug)]
 struct Bday {
@@ -9,29 +11,45 @@ struct Bday {
 }
 
 fn main() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
+    let home = var("HOME").expect("HOME environment variable not set");
+    let db_dir_path = home + "/.config/bdaylog/";
+    let db_path = db_dir_path.clone() + "data.db";
 
-    conn.execute(
-        "CREATE TABLE bdays (
+    if !Path::new(&db_dir_path).exists() {
+        std::fs::create_dir(&db_dir_path).unwrap();
+    } else {
+        println!("directory exists, skipping creation.");
+    }
+
+    let conn = Connection::open(&db_path)?;
+
+    if !Path::new(&db_path).exists() {
+        conn.execute(
+            "CREATE TABLE bdays (
             id    INTEGER PRIMARY KEY,
             name  TEXT NOT NULL,
             year  INTEGER NOT NULL,
             month TEXT NOT NULL,
             day INTEGER NOT NULL
         )",
-        (), // empty list of parameters.
-    )?;
-    let me = Bday {
-        name: "Savio".to_string(),
-        year: 2002,
-        month: "January".to_string(),
-        day: 4,
-    };
-    conn.execute(
-        "INSERT INTO bdays (name, year, month, day)
-        VALUES (?1, ?2, ?3, ?4)",
-        (&me.name, &me.year, &me.month, &me.day),
-    )?;
+            (), // empty list of parameters.
+        )?;
+    } else {
+        println!("table already created.");
+    }
+
+    // let me = Bday {
+    //     name: "Savio".to_string(),
+    //     year: 2002,
+    //     month: "January".to_string(),
+    //     day: 4,
+    // };
+
+    // conn.execute(
+    //     "INSERT INTO bdays (name, year, month, day)
+    //     VALUES (?1, ?2, ?3, ?4)",
+    //     (&me.name, &me.year, &me.month, &me.day),
+    // )?;
 
     let mut stmt = conn.prepare("SELECT name, year, month, day FROM bdays")?;
     let bday_iter = stmt.query_map([], |row| {
@@ -48,8 +66,6 @@ fn main() -> Result<()> {
             Ok(bday) => println!("{} - {} {} {}", bday.name, bday.year, bday.month, bday.day),
             Err(err) => println!("{}", err),
         }
-        // println!("{0}: {1} {2} {3}",
-        // bday.name, bday.year, bday.month, bday.day);
     }
 
     Ok(())
